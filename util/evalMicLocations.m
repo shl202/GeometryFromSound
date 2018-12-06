cb8f02b7d3a040566274332eb17b6869212f38d4
mike <shl202@ucsd.edu>
2018-12-06 15:43:12 -0800

    wrapper for 2D and 3D evaluations of point set locations

---
+++ util/evalMicLocations.m
@@ -0,0 +1,13 @@
+function [lse, R, T, isValid] = evalMicLocations(mics, mics_gt, mode)
+    if strcmpi(mode, '2D')
+        [lse, R, T, isValid] = leastSquareFitting2D(mics, mics_gt);
+    elseif strcmpi(mode, '3D')
+        [lse, R, T, isValid] = leastSquareFitting3D(mics, mics_gt);
+    elseif strcmpi(mode, '3Dto2D')
+        [~, R, T, isValid] = leastSquareFitting3D(mics, mics_gt);
+        micsRT =  R * mics + T;
+        [lse, ~, ~, ~] = leastSquareFitting2D(micsRT(1:2, :), mics_gt(1:2, :));      
+    else 
+        error(['Evaluation Mode not supported.'])
+    end
+end
