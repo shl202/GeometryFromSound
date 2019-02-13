71911807c640d89b3e96269fa8bc7dd9c1d5d05a
mike <shl202@ucsd.edu>
2019-02-12 18:01:13 -0800

    function to verify locations computed are reasonable

---
+++ util/areLocationsReasonable.m
@@ -0,0 +1,31 @@
+function alr = areLocationsReasonable(locations)
+% isLocationsReasonable 
+% @description: Sanity Check for if a computed microphone location is
+% reasonable
+%               
+% @usage: alr = areLocationsReasonable(locations)
+% @param1: locations, a 3 x nm matrix of microphone positions
+% @return1: alr, a boolean value of whether the locations are reasonable.
+%
+    alr = true;
+
+    %{
+    if( maxDistancePS(locations) > 1000 )
+        alr = false;
+    end
+    %}
+
+    [~, S, ~] = svd(locations, 'econ');
+    
+    % spread way bigger than initial positions
+    if S(1,1) > 1000
+        alr = false;
+    end
+    
+    % rank deficiency errors
+    if S(1,1) > (S(2,2) * 1000)
+        alr = false;
+    end
+
+
+end
