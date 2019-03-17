c159b7322565fc3d029b233a57b034e1dd9509a7
mike <shl202@ucsd.edu>
2019-03-17 00:08:29 -0700

    added asfs3D with ransac option and clean up code

--- src/computeMicLocations.m
+++ src/computeMicLocations.m
@@ -1,20 +1,22 @@
-function [correspondences, sample, locations] = computeMicLocations(data, config, mode)   
+function [correspondences, sample, locations] = computeMicLocations(data, config, algorithm)   
 % computeMicLocations 
 % @description: wrapper function for 2D ASFS, 3D ASFS and 3D DCOSAML
 %               algorithms, which computes the microphone locations from 
 %               time difference of arrival data
 % 
-% @usage: [correspondences, sample, locations] = computeMicLocations(data, config, mode)
+% @usage: [correspondences, sample, locations] = computeMicLocations(data, config, algorithm)
 % @param1: data, data struct which contains a matrix of time difference of 
 %          arrive data.
 % @param2: config,  a configuration struct in which one of its field
 %          specify the speed of sound
-% @param3: mode, a string which specify which algorithm to call.
-%          availible modes are: ["asfs2D", "asfs3D", and "dcosaml"]
+% @param3: algorithm, a string which specify which algorithm to call.
+%          availible algorithms are: ["asfs2D", "asfs3D", and "dcosaml"]
 % @return1: correspondences, replace holder variable for when
 %           correspondences needs to be correlated.
-% @return2: sample, replace holder variable for when RANSAC is used to
+% @return2: sample, set of consistent data found after RANSAC is used to
 %           remove outlier.
+%               sample.srcs, srcs in the sample.
+%               sample.tdoas, tdoas in the sample.
 % @return3: locations,
 %               locations.C, C matrix in ASFS (2x2 or 3x3)
 %               locations.S, S matrix used in DCOSAML (5 by ns)
@@ -26,45 +28,64 @@ function [correspondences, sample, locations] = computeMicLocations(data, config
 %                                  Mprimehat is deficient in rank or when
 %                                  nearest semi-definite matrix cannot be
 %                                  computed for Q matrix;
-%   	
-
-
-	
-    if strcmpi(mode, 'asfs2D')
+%   
+    % affine structure from sound 2D
+    if strcmpi(algorithm, 'asfs2D')
         correspondences.isValid = true;
         sample.isValid = true;
-		try
-			locations = asfs2D(data.tdoas, config.speed_of_sound);
+        try
+            locations = asfs2D(data.tdoas, config.speed_of_sound);
         catch ME
-            warning(ME.message);
-            warning("locations has been set to invalid");
-            locations.isValid = false; 	
-		end
-			
-    elseif strcmpi(mode, 'asfs') || strcmpi(mode, 'asfs3D')
-        correspondences.isValid = true;
-        sample.isValid = true;
-		try
-			locations = asfs(data.tdoas, config.speed_of_sound);
-        catch ME
-			warning(ME.message);
-            warning("locations has been set to invalid");
-            locations.isValid = false;  
-		end
-		
-		
-    elseif strcmpi(mode, 'dcosaml')
-        correspondences.isValid = true;
-        sample.isValid = true;
-		try
-			locations = dcosaml(data.tdoas, config.speed_of_sound);
-		catch ME
             warning(ME.message);
             warning("locations has been set to invalid");
             locations.isValid = false;     
         end
-	else 
-        error([mode ' is not supported.'])
+    
+    % affine structure from sound 3D
+    elseif strcmpi(algorithm, 'asfs') || strcmpi(algorithm, 'asfs3D')
+        correspondences.isValid = true;
+        sample.isValid = true;   
+        try
+            locations = asfs(data.tdoas, config.speed_of_sound);
+        catch ME
+            warning(ME.message);
+            warning("locations has been set to invalid");
+            locations.isValid = false;     
+        end
+    
+    % direct computation of source and microphone locations
+    elseif strcmpi(algorithm, 'dcosaml')
+        correspondences.isValid = true;
+        sample.isValid = true;
+        try
+            locations = dcosaml(data.tdoas, config.speed_of_sound);
+        catch ME
+            warning(ME.message);
+            warning("locations has been set to invalid");
+            locations.isValid = false;     
+        end
+    
+    % affine structure from sound 3D with ransac
+    elseif strcmpi(algorithm, 'ransacasfs') || strcmpi(algorithm, 'ransacasfs3D') 
+        correspondences.isValid = true; 
+        
+        [ns, nm] = size(data.tdoas);
+        
+        % parameters for ransac and asfs3D 
+        minCase = 9;
+        T = 3; % rmse of microphone position estimates
+        Pgood = 3/4; % estimated percentage of sound sources that are good  
+        K = floor(Pgood * ns);
+        Pfail = 0.005;
+        L = ceil(log(Pfail)/log(1-(Pgood^minCase)));
+        try
+            [~, sample, locations] = ransacMicLocations(data, config, 'asfs3D', minCase, T, K, L);
+        catch ME
+            warning(ME.message);
+            warning("locations has been set to invalid");
+            locations.isValid = false;     
+        end
+    else 
+        error([algorithm ' is not supported.'])
     end
-	
 end
