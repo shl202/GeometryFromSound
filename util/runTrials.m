da507436e66b395c0008639a0e76113933058076
mike <shl202@ucsd.edu>
2019-05-13 17:11:23 -0700

    preload track data; fixed bug with invalid locations counting

--- util/runTrials.m
+++ util/runTrials.m
@@ -1,4 +1,4 @@
-function stats = runTrials(configs, iterations, algorithm, eval_mode, seed)  
+function stats = runTrials(configs, iterations, algorithm, eval_algorithm, seed)  
     SEEDMAX = 2^31; % IMIN and IMAX must be less than 2^53.
     if ~exist('seed', 'var')
         % basically generate a random seed if no
@@ -6,11 +6,16 @@ function stats = runTrials(configs, iterations, algorithm, eval_mode, seed)
         seed = randi(SEEDMAX);
     end
     
+    addpath('../');
     addpath('../src');
     addpath('../util');
     addpath('../data');
     
     % initialize variables
+    stats.iterations = iterations;
+    stats.algorithm = algorithm;
+    stats.eval_algorithm = eval_algorithm;
+    stats.seed = seed;
     stats.mean_mses = zeros(length(configs), 1);
     stats.median_mses = zeros(length(configs), 1);
     stats.mean_rmses = zeros(length(configs), 1);
@@ -22,33 +27,43 @@ function stats = runTrials(configs, iterations, algorithm, eval_mode, seed)
     stats.num_of_invalid_samples = zeros(length(configs), 1);
     stats.num_of_invalid_locations = zeros(length(configs), 1);
     stats.num_of_invalid_reconstructions = zeros(length(configs), 1);
-    stats.max_distances = zeros(length(configs), 1);
+    %stats.max_distances = zeros(length(configs), 1);
     
     rng(seed);
-    stats.seeds = SEEDMAX * rand(length(configs), 1);
+    stats.seeds = SEEDMAX * rand(length(configs), iterations);
     
+    disp(['Algorithm: ' algorithm]);
     
     % Run the system with all the given configurations
     for c=1:length(configs)
         disp(['Configuration: ' num2str(c)]);
-        distances = zeros(iterations, 1);
+        %distances = zeros(iterations, 1);
         mses = NaN(iterations, 1);
         invalid_cor = 0; % correspondences
         invalid_sam = 0; % samples
         invalid_loc = 0; % locations
         invalid_rec = 0; % reconstructions
         
-        % seeds make sure that we can duplicate the generated data.
-        rng(stats.seeds(c));
+        % Pre-loading track file to reduce run time from loading. Loading 
+        % at each configuration.
+        tracks_file = load(configs(c).tracks_file);
+        tracks = tracks_file.tracks;
+        tracks_config_file = load(configs(c).tracks_config_file);
+        tracks_config = tracks_config_file.tracks_config;
         
         % Run the system for given iterations
         for it=1:iterations
-            disp(['Config: ' num2str(c)]);
-            disp(['  Iteration: ' num2str(it)]);
-
-            data = generateTDOAData(configs(c));
+            if mod(it, 100) == 0
+                disp(['Config: ' num2str(c)]);
+                disp(['  Iteration: ' num2str(it)]);
+            end
             
-            distances(it) = maxDistancePS(data.gt.mics);
+            % seeds make sure that we can replicate the generated data.
+            rng(stats.seeds(c, it));
+
+            data = generateTDOAData(configs(c), tracks, tracks_config);
+            
+            %distances(it) = maxDistancePS(data.gt.mics);
 
             [correspondences, sample, locations] = computeMicLocations(data, configs(c), algorithm);
                   
@@ -66,7 +81,7 @@ function stats = runTrials(configs, iterations, algorithm, eval_mode, seed)
             
             
             % check if computed result is valid
-            if (sample.isValid && locations.isValid)
+            if ( locations.isValid )
                 % computed microphone locations
                 mics = locations.mics;
                 % ground truth microphone locations
@@ -82,7 +97,7 @@ function stats = runTrials(configs, iterations, algorithm, eval_mode, seed)
                     mses(it) = NaN;   
                 else
                     % compute lse using least Square Fitting for 2D/3D point sets
-                    [lse, R, T, RTisValid] = evalMicLocations(mics, mics_gt, eval_mode);
+                    [lse, R, T, RTisValid] = evalMicLocations(mics, mics_gt, eval_algorithm);
 
                     [h, w] = size(locations.mics);
                     if (RTisValid)
@@ -97,7 +112,7 @@ function stats = runTrials(configs, iterations, algorithm, eval_mode, seed)
         
         % tally up the statistics
         validmses = mses(~isnan(mses));  
-        stats.max_distances(c) = max(distances);
+        %stats.max_distances(c) = max(distances);
         stats.mean_mses(c) = mean(validmses); 
         stats.median_mses(c) = median(validmses);
         stats.mean_rmses(c) = mean(sqrt(validmses));
